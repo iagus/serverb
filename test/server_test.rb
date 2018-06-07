ffba8906624783863d95fbb2305e20a8aa1728ca
iagus <santiago.fernandez@the-cocktail.com>
2018-06-07 22:18:01 +0100

    starts server in test itself

--- test/server_test.rb
+++ test/server_test.rb
@@ -3,8 +3,13 @@ require 'net/http'
 
 describe 'server gets GET' do
   it 'must return 200' do
-    request = Net::HTTP.new('localhost', 3000)
-    response = request.request_head('/')
-    assert response.code.to_i == 200
+    Thread.new {
+      Serverb::Server.start
+    }
+    Thread.new {
+      request = Net::HTTP.new('localhost', 3000)
+      response = request.request_head('/')
+      assert response.code.to_i == 200
+    }
   end
 end
