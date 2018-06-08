f2c7f492e4bf33f6f682a48471764b6e3e8422ea
iagus <santiago.fernandez@the-cocktail.com>
2018-06-08 21:12:43 +0100

    Includes Parser module

--- lib/serverb.rb
+++ lib/serverb.rb
@@ -1,6 +1,7 @@
 require 'serverb/version'
 require 'serverb/server'
 require 'serverb/response'
+require 'serverb/parser'
 
 module Serverb
   # Your code goes here...
---
+++ lib/serverb/parser.rb
@@ -0,0 +1,28 @@
+module Serverb
+
+  module Parser
+
+    extend self
+
+    @@REGEX = { uri: /(^\/.*(?=\?)|^\/.*)/ }
+
+    def parse_request_line req
+      method, path, protocol = req.split(' ')
+    end
+
+    def get_uri_string path
+      uri = path.split('?').first
+    end
+
+    def get_params_string path
+      params = path.split('?').last
+    end
+
+    def params_to_hash params
+      params_array = params.split('&')
+      params_array = params_array.map{ |param| param.split('=') }.to_h
+    end
+
+  end
+
+end
